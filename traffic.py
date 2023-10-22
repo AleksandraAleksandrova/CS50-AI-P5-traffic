9b675a8023fac67913373b24f6ca83b529314a33
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-10-22 13:08:57 +0300

    load data and last model

--- traffic.py
+++ traffic.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import os
 import sys
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Set TensorFlow logging level to suppress warnings
 import tensorflow as tf
 
 from sklearn.model_selection import train_test_split
@@ -58,8 +59,25 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    raise NotImplementedError
+    images = []
+    labels = []
+    for folder in os.listdir(data_dir):
+        try:
+            int(folder)
+        except ValueError:
+            print("Not a folder")
+            continue
 
+        for file in os.listdir(os.path.join(data_dir, folder)):
+            img = cv2.imread(os.path.join(data_dir, folder, file))
+            img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+            if img is None:
+                print("Could not read image")
+                continue
+            images.append(img)
+            labels.append(int(folder))
+    
+    return (images, labels)
 
 def get_model():
     """
@@ -67,8 +85,23 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(128, (2, 2), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dropout(0.3),
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])
 
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
 
 if __name__ == "__main__":
     main()
